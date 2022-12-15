514ea27faf4ab31421cdb0cf8e91006db4a97b48
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-12-15 16:27:34 -0300

    Add files via upload

---
+++ Exercicio06.java
@@ -0,0 +1,23 @@
+//6) Escreva um algoritmo para ler dois valores do teclado e exibir como resposta a divisão dos valores.
+import java.util.Scanner;
+
+public class Exercicio06 {
+	
+public static void main (String[]args) {
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Digite o primeiro valor");
+
+int n1= sc.nextInt();
+
+System.out.println("Digite o segundo valor que deseja multiplicar");
+
+int n2= sc.nextInt();
+
+System.out.println(n1/n2);
+
+	  }
+
+	}
+
